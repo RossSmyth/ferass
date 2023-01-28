31036355654ca987ba3ac96f496afa5732e31a97
Ross Smyth <crs2017@gmail.com>
2023-01-27 22:56:47 -0500

    Create libass-sys I hate submodule but oh well.

---
+++ libass-sys/build.rs
@@ -0,0 +1,11 @@
+
+fn main() {
+    /*
+    let bindings = bindgen::Builder::default()
+        .header("./libass/libass/ass.h")
+        .layout_tests(false).opaque_type("ass_library").opaque_type("ass_track").default_enum_style(bindgen::EnumVariation::ModuleConsts).allowlist_function("ass_.+").allowlist_type("ass_.+").allowlist_var("ass_.+").blocklist_type("(?i)ass_event").blocklist_type(".+[P|p]riv")
+        .generate().expect("oops");
+
+    bindings.write_to_file("./src/lib.rs").expect("owie");
+    */
+}
